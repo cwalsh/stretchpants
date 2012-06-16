4a33f2e937cb68bdefcd7e7f2a86a3ab3cb630ef
Cameron Walsh <cameron.walsh@gmail.com>
2012-06-16 12:33:46 +1000

    Basic crud works.

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -2,4 +2,19 @@ class CampersController < ApplicationController
   def index
     @campers = Camper.search "*"
   end
+  
+  def show
+    @camper = Camper.search(params[:id].to_s).first
+  end
+  
+  def new
+    @camper = Camper.new
+  end
+  
+  def create
+    @camper = Camper.create(params[:camper])
+    sleep(1)
+    redirect_to @camper
+  end
+  
 end
--- app/models/camper.rb
+++ app/models/camper.rb
@@ -5,7 +5,23 @@ class Camper
   property :created_at, :type => 'date'
   property :updated_at, :type => 'date'
   property :content
-  property :tags, :class => [String]
+  property :tags, :type => 'array'
+
+  before_save :timestamps
+
+  def id
+    title
+  end
+  
+  def tags=(tags)
+    return if tags.nil?
+    t=self.attributes[:tags] = tags.gsub(/,/," ").split
+  end
+
+  def timestamps
+    self.updated_at = Time.now
+    self.created_at ||= Time.now
+  end
 
   # If you were using ActiveRecord etc. instead you would include these:
   #include Tire::Model::Search
