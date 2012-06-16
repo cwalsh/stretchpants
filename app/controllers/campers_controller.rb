cda5bc20bc2e87b730a97d8812f97e6917a20015
Cameron Walsh <cameron.walsh@gmail.com>
2012-06-16 18:00:22 +1000

    Allow editing.

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -10,6 +10,17 @@ class CampersController < ApplicationController
   def new
     @camper = Camper.new
   end
+
+  def edit
+    @camper = Camper.find(CGI.escape(params[:id].to_s))
+  end
+
+  def update
+    params[:camper][:tags] = params[:camper][:tags].gsub(/,/, ' ').split(' ')
+    @camper = Camper.find(params[:id].to_s)
+    @camper.update_attributes(params[:camper])
+    redirect_to @camper
+  end
   
   def create
     params[:camper][:tags] = params[:camper][:tags].gsub(/,/, ' ').split(' ')
