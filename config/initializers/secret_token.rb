697cb91c46224a4cbe91be31de5c2a19a4877cd8
Cameron Walsh <cameron.walsh@gmail.com>
2012-06-16 11:19:26 +1000

    Empty app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stretchpants::Application.config.secret_token = '0af7055de8b05a5345c43d695a8680984c9eebc8f91cc021a2f0ef121da8858989bb1e7353f239bd6b846dec85055ec4d3091c0dfd28bb4c17f16086e5658700'
