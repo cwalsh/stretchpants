666508ab88f9a2b52ca58594cb342113e945d0d0
Cameron Walsh <cameron.walsh@gmail.com>
2012-06-21 21:30:38 +1000

    Use debug level logging while learning about elastic search.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Stretchpants::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.log_level = :debug
 end
+
+Tire.configure { logger 'log/elasticsearch.log', :level => 'debug' }
